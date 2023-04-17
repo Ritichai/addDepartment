602f5eecf5956e0683a2b4fdb289605daf675aed
wanchalerm.junsong <wanchalerm.junsong@gmail.com>
2023-04-17 21:02:05 +0700

    แก้ไข page not found 404 error in wordpress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, RoutesRecognized } from '@angular/router';
 import Swal from 'sweetalert2'
 
 @Component({
@@ -8,7 +9,31 @@ import Swal from 'sweetalert2'
 })
 export class AppComponent implements OnInit {
   title = 'FrontendV2';
-  ngOnInit() : void{
+  backUrl: any = [];
+  constructor(
+    private router: Router
+  ) {
+    this.router.events.subscribe((val: any) => {
+      if (val instanceof RoutesRecognized) {
+        if (val.state.url == '/login') {
+          localStorage.removeItem('backUrl');
+        }
+
+        if (localStorage.getItem('backUrl') == null) {
+          localStorage.setItem('backUrl', JSON.stringify({ currentURL: '', previousURL: '' }));
+        }
+
+        let temp: any = localStorage.getItem('backUrl');
+        this.backUrl = JSON.parse(temp.toString());
+        this.backUrl.previousURL = this.backUrl.currentURL;
+        this.backUrl.currentURL = val.state.url;
+        localStorage.setItem('backUrl', JSON.stringify(this.backUrl));
+
+      }
+    });
+  }
+
+  ngOnInit(): void {
     // Swal.fire('Hello world!');
   }
 }
