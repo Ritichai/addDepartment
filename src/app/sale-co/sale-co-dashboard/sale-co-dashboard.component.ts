d20020e18e525fc8edf45faf1f386104f8cc4037
Ritichai jaturapark <kewkew5588@gmail.com>
2023-04-19 14:04:13 +0700

    แสดงข้อมูลในตาราง สองชั้นได้แล้ว

--- src/app/sale-co/sale-co-dashboard/sale-co-dashboard.component.ts
+++ src/app/sale-co/sale-co-dashboard/sale-co-dashboard.component.ts
@@ -5,132 +5,63 @@ import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
 import { OpenSaleforecastComponent } from '../open-saleforecast/open-saleforecast.component'
+import { animate, state, style, transition, trigger } from '@angular/animations';
+
 @Component({
   selector: 'app-sale-co-dashboard',
   templateUrl: './sale-co-dashboard.component.html',
-  styleUrls: ['./sale-co-dashboard.component.scss']
+  styleUrls: ['./sale-co-dashboard.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ]
 })
 export class SaleCoDashboardComponent {
-
-  dataSaleCo = [
+  expandedElement: saleCoModel | null = null;
+  data = [
     {
       id: 1,
       month: "12/2565",
-      list_data: "แผนการขายล่วงหน้า คร้้งที่ 1",
-      type_sale_co: 1,
-      status: "วางแผนวัตถุดิบ",
-      createAt: "",
-      dueDate: "",
-      approval_data: ""
-    },{
+      dataSaleForeCast: [
+        {
+          id: 1,
+          list_data: "แผนการขายล่วงหน้า คร้้งที่ 1",
+          type_sale_co: 1,
+          status: "วางแผนวัตถุดิบ",
+          createAt: "",
+          dueDate: "",
+          approval_data: ""
+        },
+        {
+          id:3,
+          list_data: "แผนการขายล่วงหน้า คร้้งที่ 2",
+          type_sale_co: 1,
+          status: "วางแผนวัตถุดิบ",
+          createAt: "",
+          dueDate: "",
+          approval_data: ""
+        }
+      ],
+    }, {
       id: 2,
       month: "11/2565",
-      list_data: "",
-      type_sale_co: 2,
-      status: "อนุมัติสั่งซื้อวัตถุดิบ",
-      createAt: "",
-      dueDate: "",
-      approval_data: ""
-    },{
-      id: 3,
-      month: "10/2565",
-      list_data: "",
-      type_sale_co:3,
-      status: "อนุมัติผลิต",
-      createAt: "",
-      dueDate: "",
-      approval_data: ""
-    },{
-      id: 4,
-      month: "9/2565",
-      list_data: "",
-      type_sale_co:1,
-      status: "ผลิต",
-      createAt: "",
-      dueDate: "",
-      approval_data: ""
+      dataSaleForeCast: [
+        {
+          id: 2,
+          list_data: "แผนการขายล่วงหน้า คร้้งที่ 1",
+          type_sale_co: 2,
+          status: "อนุมัติสั่งซื้อวัตถุดิบ",
+          createAt: "",
+          dueDate: "",
+          approval_data: ""
+        }
+      ],
     },
-    {
-      id: 5,
-      month: "8/2565",
-      list_data: "",
-      type_sale_co:2,
-      status: "เสร็จ",
-      createAt: "",
-      dueDate: "",
-      approval_data: ""
-    },
-    {
-      id: 6,
-      month: "7/2565",
-      list_data: "แผนการขายล่วงหน้า คร้้งที่ 1",
-      type_sale_co:1,
-      status: "เสร็จ",
-      createAt: "1/1/65",
-      dueDate: "",
-      approval_data: ""
-    },
-    {
-      id: 7,
-      month: "6/2565",
-      list_data: "อนุมัติแผนการสั่งซื้อวัตถุดิบ",
-      type_sale_co:2,
-      status: "เสร็จ",
-      createAt: "2/2/65",
-      dueDate: "",
-      approval_data: ""
-    },
-    {
-      id: 8,
-      month: "5/2565",
-      list_data: "อนุมัติแผนการสั่งซื้อวัตถุดิบ",
-      type_sale_co:2,
-      status: "เสร็จ",
-      createAt: "2/2/65",
-      dueDate: "",
-      approval_data: ""
-    },
-    {
-      id: 9,
-      month: "4/2565",
-      list_data: "อนุมัติแผนการสั่งซื้อวัตถุดิบ",
-      type_sale_co:2,
-      status: "เสร็จ",
-      createAt: "2/2/65",
-      dueDate: "",
-      approval_data: ""
-    },
-    {
-      id: 10,
-      month: "3/2565",
-      list_data: "อนุมัติแผนการสั่งซื้อวัตถุดิบ",
-      type_sale_co:2,
-      status: "เสร็จ",
-      createAt: "2/2/65",
-      dueDate: "",
-      approval_data: ""
-    },
-    {
-      id: 11,
-      month: "2/2565",
-      list_data: "อนุมัติแผนการสั่งซื้อวัตถุดิบ",
-      type_sale_co:2,
-      status: "เสร็จ",
-      createAt: "2/2/65",
-      dueDate: "",
-      approval_data: ""
-    },
-    {
-      id: 12,
-      month: "1/2565",
-      list_data: "อนุมัติแผนการสั่งซื้อวัตถุดิบ",
-      type_sale_co:2,
-      status: "เสร็จ",
-      createAt: "2/2/65",
-      dueDate: "",
-      approval_data: ""
-    }
   ];
+
   columnsSalecoTable: string[] = [
     "month",
     "list_data",
@@ -142,6 +73,7 @@ export class SaleCoDashboardComponent {
     "action",
     "view"
   ];
+
   dataSourceOfSaleCoTable = new MatTableDataSource<saleCoModel>();
   //columnsToDisplayWithExpand = [...this.columnsHolidayTable, 'expand'];
   @ViewChild(MatPaginator, { static: true }) MatPaginator!: MatPaginator;
@@ -154,9 +86,9 @@ export class SaleCoDashboardComponent {
   ngOnInit(): void {
     this.dataSourceOfSaleCoTable.paginator = this.MatPaginator;
     this.dataSourceOfSaleCoTable.sort = this.MatSort;
-    this.dataSourceOfSaleCoTable.data = [
-      ...this.dataSaleCo
-    ];
+
+    console.log(this.data);
+    this.dataSourceOfSaleCoTable.data = this.data;
 
   }
 
@@ -172,11 +104,6 @@ export class SaleCoDashboardComponent {
 export interface saleCoModel {
   id: number;
   month: string;
-  list_data: string;
-  type_sale_co: number;
-  status: string;
-  createAt: string;
-  dueDate: string;
-  approval_data: string;
+  dataSaleForeCast:object;
 }
 
